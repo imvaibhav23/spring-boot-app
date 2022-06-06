7a547bf49f2dd496b9bc054ce19590585824340e
Vaibhav Srivastava <srivastava.vaibhav23@gmail.com>
2022-06-07 04:13:37 +0530

    Intial h2dabase Code

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Alien {
+    @Id
+    public int id;
+    public String name;
+    public String tech;
+
+    public Alien(){
+        super();
+        System.out.print("object created");
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+}
